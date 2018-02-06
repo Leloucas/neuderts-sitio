dc9450a285ef18cccdeedbed8bbf8fdb17cfc316
Lucas Gonzalo <lucasg.ruizd@gmail.com>
2018-02-05 23:35:47 -0700

    Se cambió el ambiente

--- neuderts/neuderts/settings.py
+++ neuderts/neuderts/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't*v!=y+-xgdgpcye4tmwdkpwzzyh7s-kna4t21tc36%3!60_rt'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
